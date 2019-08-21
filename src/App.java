3c9afa202e4e47b11715b13a7ebcc305c5aeef8f
Pratik_Singhan <pratik_singhan@persistent.com>
2019-08-21 15:14:04 +0530

    Added some more text

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java --I am awesome!!!!");
-        System.out.println("I am not bored with the training gfgh");
+        System.out.println("I am not bored with the training  gsdfh rgdfh");
     }
 }
