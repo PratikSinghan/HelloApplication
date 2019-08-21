069c644670fb4b37aea2952af8446995b8aacf25
Pratik_Singhan <pratik_singhan@persistent.com>
2019-08-21 14:38:18 +0530

    I am not bored

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java --I am awesome!!!!");
+        System.out.println("I am not bored with the training");
     }
 }
